c34e5dba21296eb2850f212a5a1c17926b930e35
Hugo Aguero <hugo.aguero@softonitg.com>
2017-01-10 09:11:58 -0600

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,41 @@
+//const MongoClient = require('mongodb').MongoClient;
+
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+
+MongoClient.connect ('mongodb://localhost:27017/TodoApp', (err,db)=>{
+    if (err){
+       return  console.log('Unable to connect to MongoDB server')
+    }
+
+    console.log('Connected to MongoDB server');
+
+     /*db.collection ('Todos').deleteMany ({text: 'Eat lunch'}).then ((result)=>{
+         console.log (result);
+     }) ;*/
+
+     /*db.collection('Todos').deleteOne({text: 'Eat lunch'}).then ((result)=>{
+         console.log (result);
+     });*/
+
+     /*db.collection('Todos').findOneAndDelete({completed: false}).then((result ) => {
+         console.log (result);
+     });*/
+
+     /*db.collection ('Users').deleteMany ({name: 'Hugo Aguero'}).then ((result)=>{
+         console.log (result);
+     });*/
+
+     db.collection ('Users').deleteOne ({_id : new ObjectID('5874dce68ee6a52e8ac1298f')}).then ( (result)=>{
+         console.log (result);
+     });
+
+
+
+
+    
+    //db.close();
+
+     
+});
